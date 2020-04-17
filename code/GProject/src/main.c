1b827afe16e093b5921b375116839c381f50e763
Pierre YURI <pierre.yuri@outlook.fr>
2020-04-17 22:43:05 +0200

    Initial Commit

--- code/GProject/src/main.c
+++ code/GProject/src/main.c
@@ -3,7 +3,6 @@
 //===============================================
 int main(int argc, char** argv) {
 	GProcess()->Process(argc, argv);
-	GProcess()->Delete();
 	return 0;
 }
 //===============================================
--- code/GProject/src/manager/GProcess.c
+++ code/GProject/src/manager/GProcess.c
@@ -1,32 +1,35 @@
 //===============================================
 #include "GProcess.h"
+#include "GShell.h"
 #include "GDebug.h"
 //===============================================
 static GProcessO* m_GProcessO = 0;
 //===============================================
 static void GProcess_Process(int argc, char** argv);
+//===============================================
 static void GProcess_Test(int argc, char** argv);
+static void GProcess_Help();
 //===============================================
 GProcessO* GProcess_New() {
 	GDebug()->Write(__FUNCTION__, 0);
-    GProcessO* lObj = (GProcessO*)malloc(sizeof(GProcessO));
-    lObj->Delete = GProcess_Delete;
-    lObj->Process = GProcess_Process;
-    return lObj;
+	GProcessO* lObj = (GProcessO*)malloc(sizeof(GProcessO));
+	lObj->Delete = GProcess_Delete;
+	lObj->Process = GProcess_Process;
+	return lObj;
 }
 //===============================================
 void GProcess_Delete() {
 	GDebug()->Write(__FUNCTION__, 0);
-    GProcessO* lObj = GProcess();
-    free(lObj);
-    m_GProcessO = 0;
+	GProcessO* lObj = GProcess();
+	free(lObj);
+	m_GProcessO = 0;
 }
 //===============================================
 GProcessO* GProcess() {
-    if(m_GProcessO == 0) {
-        m_GProcessO = GProcess_New();
-    }
-    return m_GProcessO;
+	if(m_GProcessO == 0) {
+		m_GProcessO = GProcess_New();
+	}
+	return m_GProcessO;
 }
 //===============================================
 static void GProcess_Process(int argc, char** argv) {
@@ -39,9 +42,24 @@ static void GProcess_Process(int argc, char** argv) {
 		}
 		break;
 	}
+	if(lRunFlag == 0) GProcess_Help();
 }
 //===============================================
 static void GProcess_Test(int argc, char** argv) {
 	GDebug()->Write(__FUNCTION__, 0);
+	GShell()->Test(argc, argv);
+}
+//===============================================
+static void GProcess_Help() {
+	GDebug()->Write(__FUNCTION__, 0);
+	const char* lModule = "gp_c";
+	printf("\n");
+	printf("%s\n", "Description:");
+	printf("\t%s\n", "Operations sur le module gp_c.");
+	printf("\n");
+	printf("%s\n", "Utilisation:");
+	printf("\t\%s : %s\n", lModule, "afficher aide");
+	printf("\t\%s %s : %s\n", lModule, "test", "tester module");
+	printf("\n");
 }
 //===============================================
