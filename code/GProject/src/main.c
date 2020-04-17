9ea35b57b80d51a80d0f9b6781c472253aa8af1e
Pierre YURI <pierre.yuri@outlook.fr>
2020-04-17 21:32:36 +0200

    Initial Commit

--- code/GProject/src/main.c
+++ code/GProject/src/main.c
@@ -1,13 +1,8 @@
 //===============================================
-#include "GSetting.h"
-#include "GOptions.h"
-#include "GProcess.h"
+#include <stdio.h>
 //===============================================
 int main(int argc, char** argv) {
-    GSetting()->Load("data/config/config.txt");
-    GOptions()->GetArgs(argc, argv);
-    GProcess()->Run(argc, argv);
-    GProcess()->Delete();
-    return 0;
+	printf("ooooooooooo\n");
+	return 0;
 }
 //===============================================
