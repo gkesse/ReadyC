869fbfb2a64c575d8633f4274b0703e4a07445ef
Gerard KESSE <tiakagerard@hotmail.com>
2020-09-06 09:47:58 -0400

    Initial Commit

--- test/c_date/code/GProject/src/main.c
+++ test/c_date/code/GProject/src/main.c
@@ -6,24 +6,10 @@
 void GDate_Read(char* buffer);
 //===============================================
 int main(int argc, char** argv) {
-  int h, min, s, day, mois, an;
-  time_t now;
-    
-  // Renvoie l'heure actuelle
-  time(&now);
-  // Convertir au format heure locale
-  printf("Aujourd'hui est : %s", ctime(&now));
-  struct tm *local = localtime(&now);
-  h = local->tm_hour;        
-  min = local->tm_min;       
-  s = local->tm_sec;       
-  day = local->tm_mday;          
-  mois = local->tm_mon + 1;     
-  an = local->tm_year + 1900;  
-  printf("L'heure : %02d:%02d:%02d\n", h, min, s);
-  // Afficher la date courante
-  printf("La date : %02d/%02d/%d\n", day, mois, an);
-  return 0;
+    char lDate[256];
+    GDate_Read(lDate);
+    printf("%-20s : %s\n", "DATE", lDate);
+    return 0;
 }
 //===============================================
 void GDate_Read(char* buffer) {
@@ -36,6 +22,7 @@ void GDate_Read(char* buffer) {
     int lHour = lTimeInfo->tm_hour;
     int lMin = lTimeInfo->tm_min;
     int lSec = lTimeInfo->tm_sec;
+    if(lTimeInfo->tm_isdst == 1) lHour++;
     sprintf(buffer, "%02d/%02d/%04d %02d:%02d:%02d", lDay, lMonth, lYear, lHour, lMin, lSec);
 }
 //===============================================
