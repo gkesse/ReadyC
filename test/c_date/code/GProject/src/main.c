27486905fd1d866ae5ecf35400da7ee183678c22
Gerard KESSE <tiakagerard@hotmail.com>
2020-09-14 20:25:54 +0200

    Initial Commit

--- test/c_date/code/GProject/src/main.c
+++ test/c_date/code/GProject/src/main.c
@@ -13,12 +13,21 @@ const char * G_DAY_MAP[] = {
 };
 //===============================================
 void GDate_DateTime(char* buffer);
-void GDate_Full(char* buffer);
+void GDate_DayName(char* buffer);
+void GDate_MonthName(char* buffer);
 //===============================================
 int main(int argc, char** argv) {
     char lDate[256];
+    char lDay[256];
+    char lMonth[256];
+    
     GDate_DateTime(lDate);
+    GDate_DayName(lDay);
+    GDate_MonthName(lMonth);
+    
     printf("%-20s : %s\n", "DATE", lDate);
+    printf("%-20s : %s\n", "JOUR", lDay);
+    printf("%-20s : %s\n", "MOIS", lMonth);
     return 0;
 }
 //===============================================
@@ -32,21 +41,23 @@ void GDate_DateTime(char* buffer) {
     int lHour = lTimeInfo->tm_hour;
     int lMin = lTimeInfo->tm_min;
     int lSec = lTimeInfo->tm_sec;
-    if(lTimeInfo->tm_isdst > 1) lHour =+ lTimeInfo->tm_isdst;
+    if(lTimeInfo->tm_isdst > 0) lHour =+ lTimeInfo->tm_isdst;
     sprintf(buffer, "%02d/%02d/%04d %02d:%02d:%02d", lDay, lMonth, lYear, lHour, lMin, lSec);
 }
 //===============================================
-void GDate_DateTime(char* buffer) {
+void GDate_DayName(char* buffer) {
     time_t lRawTime;
     time(&lRawTime);
     struct tm* lTimeInfo = localtime(&lRawTime);
-    int lDay = lTimeInfo->tm_mday;
-    int lMonth = lTimeInfo->tm_mon + 1;
-    int lYear = lTimeInfo->tm_year + 1900;
-    int lHour = lTimeInfo->tm_hour;
-    int lMin = lTimeInfo->tm_min;
-    int lSec = lTimeInfo->tm_sec;
-    if(lTimeInfo->tm_isdst > 1) lHour =+ lTimeInfo->tm_isdst;
-    sprintf(buffer, "%02d/%02d/%04d %02d:%02d:%02d", lDay, lMonth, lYear, lHour, lMin, lSec);
+    int lWeekDay = lTimeInfo->tm_wday;
+    sprintf(buffer, "%s", G_DAY_MAP[lWeekDay]);
+}
+//===============================================
+void GDate_MonthName(char* buffer) {
+    time_t lRawTime;
+    time(&lRawTime);
+    struct tm* lTimeInfo = localtime(&lRawTime);
+    int lMonth = lTimeInfo->tm_mon;
+    sprintf(buffer, "%s", G_MONTH_MAP[lMonth]);
 }
 //===============================================
