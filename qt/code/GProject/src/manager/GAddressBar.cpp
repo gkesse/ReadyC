682bef7bd40e0f76198cd01791496b60fde69e14
Gerard KESSE <tiakagerard@hotmail.com>
2020-10-22 08:15:19 +0200

    Initial Commit

--- qt/code/GProject/src/manager/GAddressBar.cpp
+++ qt/code/GProject/src/manager/GAddressBar.cpp
@@ -9,21 +9,23 @@ GAddressBar::GAddressBar(QWidget* parent) : GWidget(parent) {
     m_icon->setObjectName("icon");
     m_icon->setIcon(GManager::Instance()->loadPicto(fa::home, "white"));
     m_icon->setIconSize(QSize(25, 25));
+    m_icon->setCursor(Qt::PointingHandCursor);
 
     m_address = new QLineEdit;
     m_address->setObjectName("address");
 
     m_goto = new QPushButton;
     m_goto->setObjectName("goto");
-    m_goto->setIcon(GManager::Instance()->loadPicto(fa::home, "white"));
+    m_goto->setIcon(GManager::Instance()->loadPicto(fa::paperplaneo, "white"));
     m_goto->setIconSize(QSize(25, 25));
+    m_goto->setCursor(Qt::PointingHandCursor);
 
     m_mainLayout = new QHBoxLayout;
     m_mainLayout->addWidget(m_icon);
     m_mainLayout->addWidget(m_address, 1);
     m_mainLayout->addWidget(m_goto);
     m_mainLayout->setMargin(0);
-    m_mainLayout->setSpacing(0);
+    m_mainLayout->setSpacing(5);
 
     setLayout(m_mainLayout);
 }
